4de86119825666141fe50c7cbb3d578d472b53ac
codenoob66 <167981239+codenoob66@users.noreply.github.com>
2024-05-06 22:08:04 +0800

    Updates

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let player1Score = 0;
 let player2Score = 0;
+let game = ["rock", "paper", "scissors"];
+let playerImage1 = document.getElementById("playerImage1");
+let playerImage2 = document.getElementById("playerImage2");
 
-let game = ["rock", "paper", "scicors"];
+function player1Roll() {
+  let roll = Math.floor(Math.random() * 3);
+  playerImage1.textContent = game[roll];
+  return game[roll];
+}
 
-console.log(game);
+function player2Roll() {
+  console.log("player2Roll called");
+  let roll = Math.floor(Math.random() * 3);
+  playerImage2.textContent = game[roll];
+  console.log("Player 2 rolled: " + game[roll]);
+  return game[roll];
+}
