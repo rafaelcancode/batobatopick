7e1e7d5993111026e6dd0400c9ad62a9d29e00ed
codenoob66 <167981239+codenoob66@users.noreply.github.com>
2024-05-07 16:40:37 +0800

    Fixed errors

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-let player1Score = 0;
-let player2Score = 0;
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-let game = ["rock", "paper", "scicors"];
+const STAR_COUNT = 400;
+let result = "";
+for (let i = 0; i < STAR_COUNT; i++) {
+  result += `${randomNumber(-50, 50)}vw ${randomNumber(
+    -50,
+    50
+  )}vh ${randomNumber(0, 1)}px ${randomNumber(0, 1)}px #fff,`;
+}
+document.querySelector(".main").style.boxShadow = result.substring(
+  0,
+  result.length - 1
+);
 
-console.log(game);
+/* let player1 = document.getElementById("player1");
+let player2 = document.getElementById("player2"); */
+let resultParagraph1 = document.getElementById("player1"); // Get the <p> element for player 1
+let resultParagraph2 = document.getElementById("player2"); // Get the <p> element for player 2
+
+let choices = ["rock", "paper", "scissors"];
+
+function gameFunction() {
+  let randomIndex = Math.floor(Math.random() * 3);
+  let randomChoice = choices[randomIndex]; // Accessing the random choice from the array
+  if (event.target.textContent === "Player 1 roll") {
+    resultParagraph1.textContent = randomChoice; // Set the text content of the <p> element for player 1
+  } else {
+    resultParagraph2.textContent = randomChoice; // Set the text content of the <p> element for player 2
+  }
+}
+
+function resetGame() {
+  window.location.reload();
+}
